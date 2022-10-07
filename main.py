9bfba1bf8ef3180ecd437c5a6e6dcc8533d8610a
Hrishikesh Bagalkote <Hrishikesh Bagalkote>
2022-10-07 17:53:06 -0400

    tests and rename

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import dataframe
 import numerical_feature_converter
 import data_sampler
-import common_model
+import common_model_lib
 import prediction_engine
 
 df = dataframe.get_dataframe('data/drug200.csv')
 int_map = numerical_feature_converter.int_map(df)
 df_converted = numerical_feature_converter.convert_dataframe(df, int_map)
-df_sampled = data_sampler.sampled_dataframe(df_converted, 0.8)
+df_sampled = data_sampler.sampled_dataframe(df_converted, 0.2)
+for point in df_sampled:
+    print(point)
