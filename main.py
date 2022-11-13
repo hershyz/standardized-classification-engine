4dfe358860242a4c2d9977564bb8a90b576eef7f
Hrishikesh Bagalkote <Hrishikesh Bagalkote>
2022-11-13 15:10:48 -0500

    benchmark data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import training_engine
 
-training_engine.get_model('data/healthcare-dataset-stroke-data.csv')
+# training_engine.get_model('data/healthcare-dataset-stroke-data.csv')
 
-# training_engine.get_model('benchmark-data/glass.csv')
+training_engine.get_model('benchmark-data/stars.csv')
