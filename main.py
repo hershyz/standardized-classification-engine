99f0429c8b1ce376e6e8c37dc0ba48459cb97810
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2022-11-18 14:06:31 -0500

    tests

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import common_model_lib
 import prediction_engine
 
 model = common_model_lib.parse_model('test.mlmodel')
-point_raw = '130.0,0.005,0.469,0.005,0.004,0.0,0.001,29.0,1.7,0.0,7.8,112.0,65.0,177.0,6.0,1.0,133.0,129.0,133.0,27.0,0.0,1.0'
-print(prediction_engine.predict(point_raw.split(','), model))
+point = '33,3.0,18.0,2.0,?,?,?,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0,?,?,0,129,0,0,1,1,1,1'
+print(prediction_engine.predict(point.split(','), model))
