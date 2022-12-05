0aaf35f4ae60f90a3800a2e8f792f9a56c97f89c
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2022-12-05 11:35:14 -0500

    remove pretrained models

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import common_model_lib
-import dataframe
-import numerical_feature_converter
+import training_engine
 
-model = common_model_lib.parse_model('drug200.mlmodel')
-df_raw = dataframe.get_dataframe('data/drug200.csv')
-int_map = numerical_feature_converter.int_map(df_raw)
-df_converted = numerical_feature_converter.convert_dataframe(df_raw, int_map)
-print(df_converted)
-test_accuracy = common_model_lib.test_model(model, df_converted)
-print('test accuracy: ' + str(test_accuracy))
+model = training_engine.get_model('data/drug200.csv')
